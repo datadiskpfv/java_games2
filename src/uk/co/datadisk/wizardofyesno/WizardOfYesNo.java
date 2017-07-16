a369780d442049f076c714dc723426900a90cb29
Paul Valle <paul.valle@datadisk.co.uk>
2017-07-16 21:01:44 +0100

    First game wizard of yes and no - using Swing/Events

---
+++ src/uk/co/datadisk/wizardofyesno/WizardOfYesNo.java
@@ -0,0 +1,14 @@
+package uk.co.datadisk.wizardofyesno;
+
+/**
+ * Created by vallep on 16/07/2017.
+ *
+ */
+public class WizardOfYesNo {
+    public WizardOfYesNo() {
+    }
+
+    public static void main(String[] args) {
+        new WizardOfYesNo();
+    }
+}
