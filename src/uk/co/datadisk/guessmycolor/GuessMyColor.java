b7fe01c69eea6f1b943baf11860ce564966617a0
Paul Valle <paul.valle@datadisk.co.uk>
2017-07-18 20:44:44 +0100

    Second Game Guess My Color

--- src/uk/co/datadisk/guessmycolor/GuessMyColor.java
+++ src/uk/co/datadisk/guessmycolor/GuessMyColor.java
@@ -1,4 +1,31 @@
 package uk.co.datadisk.guessmycolor;
 
-public class GuessMyColor {
+import javax.swing.*;
+import java.awt.*;
+
+public class GuessMyColor extends JFrame {
+    public GuessMyColor() {
+
+        setTitle("Guess My Color");
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+        //setSize(400, 100);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String className = UIManager.getCrossPlatformLookAndFeelClassName();
+            UIManager.setLookAndFeel(className);
+        } catch (Exception e) { }
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GuessMyColor();
+            }
+        });
+    }
 }
