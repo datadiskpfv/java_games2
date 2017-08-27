2b83a54000c6d23c21c5f957306d01417b2d0e46
Paul Valle <paul.valle@datadisk.co.uk>
2017-08-27 15:38:03 +0100

    finished game

--- src/uk/co/datadisk/framed/Framed.java
+++ src/uk/co/datadisk/framed/Framed.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Framed extends JFrame {
     private static final long serialVersionUID = 1L;
@@ -22,6 +23,7 @@ public class Framed extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        newGame();
     }
 
     private void initGUI() {
@@ -42,6 +44,8 @@ public class Framed extends JFrame {
                         int row = button.getRow();
                         int col = button.getCol();
                         toggleLights(row, col);
+
+                        endGameIfDone();
                     }
                 });
                 centerPanel.add(lightButton[row][col]);
@@ -49,8 +53,89 @@ public class Framed extends JFrame {
         }
     }
 
+    private void newGame() {
+
+        for (int row=0; row<GRIDSIZE; row++) {
+            for (int col=0; col<GRIDSIZE; col++) {
+                lightButton[row][col].toggle();
+            }
+        }
+        lightButton[1][1].toggle();
+
+        Random rand = new Random();
+        int numberOfTimes = rand.nextInt(10) + 10;
+        for(int i = 0; i < numberOfTimes; i++) {
+            int row = rand.nextInt(2);
+            int col = rand.nextInt(2);
+            toggleLights(row, col);
+        }
+    }
+
+    private void endGameIfDone() {
+        boolean done = lightButton[0][0].isLit()
+                && lightButton[0][1].isLit()
+                && lightButton[0][2].isLit()
+                && lightButton[1][0].isLit()
+                && lightButton[1][1].isLit()
+                && lightButton[1][2].isLit()
+                && lightButton[2][0].isLit()
+                && lightButton[2][1].isLit()
+                && lightButton[2][2].isLit();
+
+        if(done) {
+            String message = "Congratulations! You won! Do you want to play again?";
+            int option = JOptionPane.showConfirmDialog(this, message, "Play again?", JOptionPane.YES_NO_OPTION);
+            if (option == JOptionPane.YES_OPTION) {
+                newGame();
+            } else {
+                System.exit(0);
+            }
+        }
+    }
+
     private void toggleLights(int row, int col) {
+
         lightButton[row][col].toggle();
+
+        if (row == 0 && col == 0 ) {
+            lightButton[row][col+1].toggle();
+            lightButton[row+1][col+1].toggle();
+            lightButton[row+1][col].toggle();
+        } else if (row == 0 && col == 2 ) {
+            lightButton[row][col-1].toggle();
+            lightButton[row+1][col-1].toggle();
+            lightButton[row+1][col].toggle();
+        } else if (row == 2 && col == 0 ) {
+            lightButton[row][col+1].toggle();
+            lightButton[row-1][col+1].toggle();
+            lightButton[row-1][col].toggle();
+        } else if (row == 2 && col == 2 ) {
+            lightButton[row][col-1].toggle();
+            lightButton[row-1][col-1].toggle();
+            lightButton[row-1][col].toggle();
+        } else if (row == 0 && col == 1 ) {
+            lightButton[row+1][col].toggle();
+            lightButton[row+2][col].toggle();
+
+        } else if (row == 1 && col == 2 ) {
+            lightButton[row][col-1].toggle();
+            lightButton[row][col-2].toggle();
+
+        } else if (row == 2 && col == 1 ) {
+            lightButton[row-1][col].toggle();
+            lightButton[row-2][col].toggle();
+
+        } else if (row == 1 && col == 0 ) {
+            lightButton[row][col+1].toggle();
+            lightButton[row][col+2].toggle();
+
+        } else if (row == 1 && col == 1 ) {
+            lightButton[row-1][col].toggle();
+            lightButton[row+1][col].toggle();
+            lightButton[row][col+1].toggle();
+            lightButton[row][col-1].toggle();
+
+        }
     }
 
     public static void main(String[] args) {
