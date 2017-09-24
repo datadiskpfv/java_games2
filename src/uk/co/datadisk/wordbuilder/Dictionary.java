acad2273e616cb5d8e1ee0f53b63fa2f754099c5
Paul Valle <paul.valle@datadisk.co.uk>
2017-09-24 20:12:40 +0100

    started on new word game

---
+++ src/uk/co/datadisk/wordbuilder/Dictionary.java
@@ -0,0 +1,54 @@
+package uk.co.datadisk.wordbuilder;
+
+import javax.swing.*;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    private static final String FILENAME = "enable1_3-15.txt";
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    //@SuppressWarnings("unchecked");
+    private ArrayList<String>[] wordList = (ArrayList<String>[]) new ArrayList[26];
+
+    public Dictionary() {
+        for (int i = 0; i < 26; i++) {
+            wordList[1] = new ArrayList<String>();
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(new File(FILENAME)));
+            String word = in.readLine();
+            while (word != null) {
+                char letter = word.charAt(0);
+                int list = ALPHABET.indexOf(letter);
+                wordList[list].add(word);
+                word = in.readLine();
+            }
+            in.close();
+        }
+        catch (FileNotFoundException e) {
+            String message = FILENAME + " was not found";
+            JOptionPane.showMessageDialog(null, message);
+        }
+        catch (IOException e) {
+            String message = FILENAME + " could not be opened";
+            JOptionPane.showMessageDialog(null, message);
+        }
+    }
+
+    public boolean isAWord(String word) {
+        boolean found = false;
+        word = word.toUpperCase();
+        char letter = word.charAt(0);
+        int list = ALPHABET.indexOf(letter);
+        int index = 0;
+        String word2 = "";
+        return found;
+    }
+
+    public static void main(String[] args) {
+    }
+}
