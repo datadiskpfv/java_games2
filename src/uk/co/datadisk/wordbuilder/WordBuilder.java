28763cfa500975a5a7feda289026f177c8e05188
Paul Valle <paul.valle@datadisk.co.uk>
2017-09-29 20:51:06 +0100

    built the various panels, score, button, etc

--- src/uk/co/datadisk/wordbuilder/WordBuilder.java
+++ src/uk/co/datadisk/wordbuilder/WordBuilder.java
@@ -13,10 +13,35 @@ public class WordBuilder extends JFrame {
     private static final int COLS = 12;
     private static final int MAX = 15;
 
+    private static final String FILENAME = "highScores.txt";
+    private static final Color TAN = new Color(222, 191, 168);
+    private static Font smallFont = new Font(Font.DIALOG, Font.PLAIN, 12);
+    private static Font bigFont = new Font(Font.DIALOG, Font.BOLD, 30);
+
     private LetterPanel board[][] = new LetterPanel[ROWS][COLS];
+
+    private LetterPanel played[] = new LetterPanel[MAX];
+
+    private int points = 0;
+    private int score = 0;
+    private String word = "";
+    private Dictionary dictionary = new Dictionary();
 
     private JPanel mainPanel = new JPanel();
     private JPanel boardPanel = new JPanel();
+    private JPanel scorePanel = new JPanel();
+    private JPanel playPanel = new JPanel();
+
+    private JLabel pointsTitleLabel = new JLabel("Points: ");
+    private JLabel scoreTitleLabel = new JLabel("Score: ");
+    private JLabel pointsLabel = new JLabel("0");
+    private JLabel scoreLabel = new JLabel("0");
+
+    private JButton acceptButton = new JButton("Accept");
+    private JButton undoButton = new JButton("Undo");
+    private JButton clearButton = new JButton("Clear");
+
+
 
     public WordBuilder() {
         initGUI();
@@ -33,18 +58,49 @@ public class WordBuilder extends JFrame {
         add(titleLabel, BorderLayout.PAGE_START);
 
         // main panel
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(TAN);
         add(mainPanel, BorderLayout.CENTER);
 
-
         // score panel
+        scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.X_AXIS));
+        scorePanel.setBackground(TAN);
+        mainPanel.add(scorePanel);
 
+        pointsTitleLabel.setFont(smallFont);
+        scorePanel.add(pointsTitleLabel);
+
+        pointsLabel.setFont(bigFont);
+        scorePanel.add(pointsLabel);
+
+        Dimension boxSize = new Dimension(20, 0);
+        Component box = Box.createRigidArea(boxSize);
+        scorePanel.add(box);
+
+        scoreTitleLabel.setFont(smallFont);
+        scorePanel.add(scoreTitleLabel);
+
+        scoreLabel.setFont(bigFont);
+        scorePanel.add(scoreLabel);
 
         // play panel
+        playPanel.setLayout(new GridLayout(1,MAX));
+        playPanel.setBackground(TAN);
+        mainPanel.add(playPanel);
+
+        for (int i = 0; i < MAX; i++) {
+            LetterPanel letterPanel = new LetterPanel();
+            played[i] = letterPanel;
+            playPanel.add(letterPanel);
+        }
 
 
         // board panel
         boardPanel.setBackground(Color.BLACK);
         boardPanel.setLayout(new GridLayout(ROWS, COLS));
+        int panelSize = played[0].getPanelSize();
+        Dimension maxSize = new Dimension(COLS*panelSize, ROWS*panelSize);
+        boardPanel.setMaximumSize(maxSize);
         mainPanel.add(boardPanel);
         BagOfLetters letters = new BagOfLetters();
         for (int i = 0; i < ROWS; i++) {
@@ -56,7 +112,21 @@ public class WordBuilder extends JFrame {
         }
 
         // button panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.BLACK);
+        mainPanel.add(buttonPanel, BorderLayout.PAGE_END);
 
+        acceptButton.setEnabled(false);
+        buttonPanel.add(acceptButton);
+
+        undoButton.setEnabled(false);
+        buttonPanel.add(undoButton);
+
+        clearButton.setEnabled(false);
+        buttonPanel.add(clearButton);
+
+        JButton endButton = new JButton("End Game");
+        buttonPanel.add(endButton);
 
         // listeners
     }
